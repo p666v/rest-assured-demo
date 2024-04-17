aa35fc10be52d1d5fdf22411088c6d2936102b1c
Pavel <p666v@yandex.ru>
2024-04-17 19:18:39 +0300

    Исправлены замечания

--- src/test/java/ru/buttonone/PetStoreTest.java
+++ src/test/java/ru/buttonone/PetStoreTest.java
@@ -23,7 +23,7 @@ public class PetStoreTest {
     @ValueSource(strings = {"available", "pending", "sold"})
     @DisplayName("Проверка поиска питомцев по статусу")
     public void checkPetSearchByStatus(String argument) {
-        log.info("Проверка поиска питомцев по статусу");
+        log.info(String.format("Проверка поиска питомцев по статусу %s", argument));
         given()
                 .spec(requestSpecWithStatus(argument))
                 .when()
@@ -37,7 +37,7 @@ public class PetStoreTest {
     @ValueSource(ints = {5, 9, 10})
     @DisplayName("Проверка наличия заказа по идентификатору")
     public void checkThatOrderExistsById(int argument) {
-        log.info("Проверка наличия заказа по идентификатору");
+        log.info(String.format("Проверка наличия заказа по идентификатору %d", argument));
         given()
                 .spec(requestSpecParamsId(argument))
                 .when()
@@ -51,7 +51,7 @@ public class PetStoreTest {
     @ValueSource(ints = {5, 10, 27})
     @DisplayName("Проверка питомца по идентификатору")
     public void checkPetById(int argument) {
-        log.info("Проверка питомца по идентификатору");
+        log.info(String.format("Проверка питомца по идентификатору %d", argument));
         given()
                 .spec(requestSpecParamsId(argument))
                 .when()
@@ -93,18 +93,20 @@ public class PetStoreTest {
                 .extract().body().as(Pet.class);
 
         log.info("Проверка созданного объекта на соответствие данным в POST запросе");
+        Pet petData = petStoreTestData.petData();
+
         Assertions.assertAll(
-                () -> assertEquals(petStoreTestData.petData().getId(), response.getId(),
+                () -> assertEquals(petData.getId(), response.getId(),
                         "ID указанный в теле ответа не соответствует ID в POST запросе"),
-                () -> assertEquals(petStoreTestData.categoryData(), response.getCategory(),
+                () -> assertEquals(petData.getCategory(), response.getCategory(),
                         "Category указанная в теле ответа не соответствует Category в POST запросе"),
-                () -> assertEquals(petStoreTestData.petData().getName(), response.getName(),
+                () -> assertEquals(petData.getName(), response.getName(),
                         "Name указанный в теле ответа не соответствует Name в POST запросе"),
-                () -> assertEquals(petStoreTestData.petData().getPhotoUrls(), response.getPhotoUrls(),
+                () -> assertEquals(petData.getPhotoUrls(), response.getPhotoUrls(),
                         "PhotoUrls указанный в теле ответа не соответствует PhotoUrls в POST запросе"),
-                () -> assertEquals(petStoreTestData.petData().getTags(), response.getTags(),
+                () -> assertEquals(petData.getTags(), response.getTags(),
                         "Tag указанный в теле ответа не соответствует Tag в POST запросе"),
-                () -> assertEquals(petStoreTestData.petData().getStatus(), response.getStatus(),
+                () -> assertEquals(petData.getStatus(), response.getStatus(),
                         "Status указанный в теле ответа не соответствует Status в POST запросе")
         );
         log.info("Объект соответствует данным в POST запросе");
